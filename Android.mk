c44784f4fb54028420a51c1779d4d24511588e22
Ksawlii <maja.blaszczak@proton.me>
2025-08-10 18:54:23 +0200

    a53x: Init
    Signed-off-by: Ksawlii <maja.blaszczak@proton.me>

---
+++ Android.mk
@@ -0,0 +1,23 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),a53x)
+
+$(call add-radio-file-sha1-checked,radio/fld.bin_SM-A536B,91ba4a3c26c636fd583303caa6eefef2efbc53db)
+$(call add-radio-file-sha1-checked,radio/harx.bin_SM-A536B,cfebe9e5a1475a22f1f2fe2b7872bb3b5e1336b9)
+$(call add-radio-file-sha1-checked,radio/keystorage.bin_SM-A536B,4c0f3c1b03072717fd42e5125caa2dc93f971b0e)
+$(call add-radio-file-sha1-checked,radio/ldfw.img_SM-A536B,16b345ce34e541157c0ef2e1068522de70a126bd)
+$(call add-radio-file-sha1-checked,radio/models,d801a4b996ef929738c17ec14e9e415e59894d06)
+$(call add-radio-file-sha1-checked,radio/modem.bin_SM-A536B,691d7a5fb502ebc2bc9fd1458f138695d7f74449)
+$(call add-radio-file-sha1-checked,radio/modem_debug.bin_SM-A536B,495e5f529cd52ab30ee73991f7e73b7781719009)
+$(call add-radio-file-sha1-checked,radio/sboot.bin_SM-A536B,4cd3dbb8823740587dc4ebe926df0da617662c7a)
+$(call add-radio-file-sha1-checked,radio/tzar.img_SM-A536B,2e9eb5ff33cd4d9100bcfdcafe3e5e86a9348ca8)
+$(call add-radio-file-sha1-checked,radio/tzsw.img_SM-A536B,a3754f4a3ad8dcbae9c777dc9338e8a976b07954)
+$(call add-radio-file-sha1-checked,radio/uh.bin_SM-A536B,1bf08d9a78f903195bf6a38fab25e35cb4c2747d)
+$(call add-radio-file-sha1-checked,radio/up_param.bin_SM-A536B,5bbde2b11a7c551da737e3cf5c2d232b55ed03fe)
+$(call add-radio-file-sha1-checked,radio/version_SM-A536B,db4df53c96c8d6f2b29076c4d86a308e1b13b0cf)
+
+endif
