81d1d5cde852f55716bdca08af7603e161cf1359
Majaahh <maja.blaszczak@proton.me>
2025-11-07 09:28:55 +0100

    a53x: Update blobs from A536BXXSHGYJ3
    * Bootloader blobs were not updated due to samsung adding a prop that auto locks bootloader (in sboot.bin)
    * TEEgris firmware blobs have to match bootloader version so they were pinned
    
    Change-Id: Ic3ccd3cdcf9ead5cda9dfb4ebe0687fb17737786
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

--- a53x-vendor.mk
+++ a53x-vendor.mk
@@ -9,15 +9,6 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a53x/proprietary/recovery/root/vendor/firmware/gt9895_a53x.bin:$(TARGET_COPY_OUT_RECOVERY)/root/vendor/firmware/gt9895_a53x.bin \
     vendor/samsung/a53x/proprietary/vendor/etc/floating_feature.xml:$(TARGET_COPY_OUT_VENDOR)/etc/floating_feature.xml \
     vendor/samsung/a53x/proprietary/vendor/etc/init/nxp.android.hardware.nfc@1.2-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/nxp.android.hardware.nfc@1.2-service.rc \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_bt.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_bt.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_common.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_common.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_t_wlan.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_t_wlan.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_t_wlan_t.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_t_wlan_t.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_wlan.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_wlan.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_wlan_sw.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_wlan_sw.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_wlan_t.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_wlan_t.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/mx140_wlan_t_sw.hcf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/mx140_wlan_t_sw.hcf \
-    vendor/samsung/a53x/proprietary/vendor/etc/wifi/slsi_reg_database.bin:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/slsi_reg_database.bin \
     vendor/samsung/a53x/proprietary/vendor/firmware/APBargeIn_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APBargeIn_AUDIO_SLSI.bin \
     vendor/samsung/a53x/proprietary/vendor/firmware/APBiBF_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APBiBF_AUDIO_SLSI.bin \
     vendor/samsung/a53x/proprietary/vendor/firmware/APDV_AUDIO_SLSI.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/APDV_AUDIO_SLSI.bin \
@@ -108,6 +99,15 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a53x/proprietary/vendor/firmware/txse1.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse1.bin \
     vendor/samsung/a53x/proprietary/vendor/firmware/txse2.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/txse2.bin \
     vendor/samsung/a53x/proprietary/vendor/firmware/vts.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/vts.bin \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_bt.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_bt.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_common.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_common.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_t_wlan.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_t_wlan.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_t_wlan_t.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_t_wlan_t.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_wlan.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_wlan_sw.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan_sw.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_wlan_t.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan_t.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/mx140_wlan_t_sw.hcf:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/mx140_wlan_t_sw.hcf \
+    vendor/samsung/a53x/proprietary/vendor/firmware/wifi/slsi_reg_database.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/wifi/slsi_reg_database.bin \
     vendor/samsung/a53x/proprietary/vendor/tee/SC-53C/00000000-0000-0000-0000-000000010081:$(TARGET_COPY_OUT_VENDOR)/tee/SC-53C/00000000-0000-0000-0000-000000010081 \
     vendor/samsung/a53x/proprietary/vendor/tee/SC-53C/00000000-0000-0000-0000-000000020081:$(TARGET_COPY_OUT_VENDOR)/tee/SC-53C/00000000-0000-0000-0000-000000020081 \
     vendor/samsung/a53x/proprietary/vendor/tee/SC-53C/00000000-0000-0000-0000-000000534b4d:$(TARGET_COPY_OUT_VENDOR)/tee/SC-53C/00000000-0000-0000-0000-000000534b4d \
